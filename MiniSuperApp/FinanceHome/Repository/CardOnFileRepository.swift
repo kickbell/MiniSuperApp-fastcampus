c81a8762ba16c6db3ba47b99b65159e74d185980
kickbell <dev.kickbell@gmail.com>
2023-03-20 04:39:10 +0900

    주석 되돌리기

--- MiniSuperApp/FinanceHome/Repository/CardOnFileRepository.swift
+++ MiniSuperApp/FinanceHome/Repository/CardOnFileRepository.swift
@@ -20,8 +20,8 @@ final class CardOnFileRepositoryImp: CardOnFileRepository {
     PaymentMethod(id: "0", name: "우리은행", digits: "0123", color: "#f19a38ff", isPrimary: false),
     PaymentMethod(id: "1", name: "신한카드", digits: "0545", color: "#3478f6ff", isPrimary: false),
     PaymentMethod(id: "2", name: "현대카드", digits: "5445", color: "#78c5f5ff", isPrimary: false),
-    PaymentMethod(id: "3", name: "국민은행", digits: "3775", color: "#65c466ff", isPrimary: false),
-    PaymentMethod(id: "4", name: "카카오뱅크", digits: "4554", color: "#ffcc00ff", isPrimary: false)
+//    PaymentMethod(id: "3", name: "국민은행", digits: "3775", color: "#65c466ff", isPrimary: false),
+//    PaymentMethod(id: "4", name: "카카오뱅크", digits: "4554", color: "#ffcc00ff", isPrimary: false)
   ])
   
   func addCard(info: AddPaymentMethodInfo) -> AnyPublisher<PaymentMethod, Error> {
